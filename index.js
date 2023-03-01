e2519bb63ad016fce1f24c2c29921934234ca856
Hernan Gargano <hagargano@hotmail.com>
2023-03-01 18:50:15 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cvLink = document.querySelector("a[href='#']");
+  
+    cvLink.addEventListener("click", function() {
+      document.body.style.backgroundColor = "#FFE4B5";
+    });
+  });
+  
+  
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const datosPersonalesLink = document.querySelector("a[href='#Datos_Personales']");
+  
+    datosPersonalesLink.addEventListener("click", function() {
+      document.body.style.backgroundColor = "#DDA0DD";
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const estudiosLink = document.querySelector("a[href='#estudios']");
+  
+    estudiosLink.addEventListener("click", function() {
+      document.body.style.backgroundColor = "Darkkhaki";
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const habilidadesLink = document.querySelector("a[href='#habilidades']");
+  
+    habilidadesLink.addEventListener("click", function() {
+      document.body.style.backgroundColor = "#00FA9A";
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    const experiencia_laboralLink = document.querySelector("a[href='#experiencia_laboral']");
+  
+    experiencia_laboralLink.addEventListener("click", function() {
+      document.body.style.backgroundColor = "#F4A460";
+    });
+  });
